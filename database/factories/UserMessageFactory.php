21f7c675aa6ef072b26e7c68889767b6f6c6f189
jimmy <patchapon.r@ku.th>
2021-09-10 12:26:26 +0700

    TES-23 add migrations and relations for Message

---
+++ database/factories/UserMessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserMessage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserMessageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserMessage::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_09_09_043435_create_user_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_messages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('message_id');
+            $table->integer('sender_id');
+            $table->integer('receiver_id');
+            $table->tinyInteger('seen_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
