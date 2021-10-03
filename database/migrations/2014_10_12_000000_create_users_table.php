97eccccf3d5d899ae9d303f89e6c27177d6fc6cf
jimmy <patchapon.r@ku.th>
2021-10-03 17:07:01 +0700

    role default fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role',['PLAYER','REFEREE','ADMIN'])
-                ->default('CUSTOMER');
+                ->default('PLAYER');
             $table->rememberToken();
             $table->timestamps();
         });
