11de83b5f4c0260552c07bced108579737a50abf
Gruszht6215 <kunkawee.s@ku.th>
2021-10-05 12:04:19 +0700

    update api.php add register route

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('users',\App\Http\Controllers\Api\UserController::class);
+Route::apiResource('users', \App\Http\Controllers\Api\UserController::class);
 Route::group([
-    'middleware' => 'api', 'prefix' => 'auth'
+    'middleware' => 'api',
+    'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
+    Route::post('register', [AuthController::class, 'register']);
 });
-
