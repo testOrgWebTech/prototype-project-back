da3fa6a8aecf673ac4c93a54a8b34af7e587cf2d
Gruszht6215 <kunkawee.s@ku.th>
2021-10-08 01:42:15 +0700

    add auth middleware to Api/TeamController

--- app/Http/Controllers/Api/TeamController.php
+++ app/Http/Controllers/Api/TeamController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', [
+            'except' => ['show']
+        ]);
+    }
     /**
      * Display a listing of the resource.
      *
