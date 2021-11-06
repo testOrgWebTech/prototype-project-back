cf844003422cc6cf94ac1c5243bb45ba3eab12d2
Gruszht6215 <kunkawee.s@ku.th>
2021-11-06 12:37:36 +0700

    Install Dusk

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                // ->loginAs(User::find(15))
+                ->visit('http://localhost:8080')
+                ->refresh()
+                // ->assertSee('Home');
+                // ->waitForLocation('http://localhost:8080')
+                // ->waitForReload()
+                // ->assertPathIs('/');
+                // ->assertPathBeginsWith('/home');
+                // ->assertScript('window.isLoaded')
+                // ->assertAuthenticatedAs($user);
+                ->assertUrlIs('http://localhost:8080');
+            // ->assertDialogOpened('Dialog message');
+        });
+    }
+}
