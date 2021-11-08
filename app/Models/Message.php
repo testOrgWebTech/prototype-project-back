c32a087e493f59d891ecd20037ecb0cb43aa9ffb
jimmy <patchapon.r@ku.th>
2021-11-09 04:12:03 +0700

    Changes from Banned-jimmy pull requests

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -15,4 +15,7 @@ class Message extends Model
     public function receiver() {
         return $this->belongsTo(User::class,'receiver_id');
     }
+    public function images(){
+        return $this->hasMany(Image::class, 'image_id');
+    }
 }
